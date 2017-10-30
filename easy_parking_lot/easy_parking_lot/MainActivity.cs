2223c1d545f74d19008d18ebf5893551b713b9c7
Fenix <fantasma_001_001@hotmail.com>
2017-10-30 14:51:27 -0500

    New proyejt

---
+++ easy_parking_lot/easy_parking_lot/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace easy_parking_lot
+{
+    [Activity(Label = "easy_parking_lot", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
